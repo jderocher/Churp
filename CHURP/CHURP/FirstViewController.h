e8ff7af111cfc55e0b39894b1ee9f99aeb082616
Kylee MacLean <k.maclean@outlook.com>
2014-03-17 09:32:36 -0700

    Initial Commit

---
+++ CHURP/CHURP/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  CHURP
+//
+//  Created by Kylee MacLean on 3/17/14.
+//  Copyright (c) 2014 Whitworth University Software Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ CHURP/CHURP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CHURP
+//
+//  Created by Kylee MacLean on 3/17/14.
+//  Copyright (c) 2014 Whitworth University Software Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
