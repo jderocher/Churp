0b460af7890e6705f31f1ddda161b984c2ce5249
Kylee MacLean <k.maclean@outlook.com>
2014-03-17 16:43:58 -0700

    Here is the very very intro to CHURP app
    3/17/14

---
+++ CHURP/CHURP/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  CHURP
+//
+//  Created by Kylee MacLean on 3/17/14.
+//  Copyright (c) 2014 Whitworth University Software Engineering. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
